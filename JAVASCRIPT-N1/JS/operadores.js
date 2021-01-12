b99037db82e21418320767131224a0173a05f695
julieta96 <julietaextra14@gmail.com>
2021-01-12 13:25:32 -0300

    operadores

---
+++ JAVASCRIPT-N1/JS/operadores.js
@@ -0,0 +1,41 @@
+let numero=10;
+
+numero**=2; /*asignacion exponencial*/
+
+document.write(numero); 
+
+/*ASI SI*/
+num1 = 10;
+
+num1--;
+ 
+resultado = num1; /*la resta se hace despues de q se procese eso, se cambia despues de la ejecucion*/
+
+document.write("   "+resultado);
+
+
+/*ASI NO*/
+
+num1 = 10;
+
+resultado = num1--; /*la resta se hace despues de q se procese eso, se cambia despues de la ejecucion*/
+
+document.write("   "+resultado);
+
+/*ASI SI*/
+num1 = 10;
+
+num1++;
+ 
+resultado = num1; /*la resta se hace despues de q se procese eso, se cambia despues de la ejecucion*/
+
+document.write("   "+resultado);
+
+
+/*ASI NO*/
+
+num1 = 10;
+
+resultado = num1++; /*la resta se hace despues de q se procese eso, se cambia despues de la ejecucion*/
+
+document.write("   "+resultado);
