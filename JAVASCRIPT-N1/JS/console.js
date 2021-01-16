3e4fbc94aa804214cb84ca6894d7b478bf0dfb08
julieta96 <julietaextra14@gmail.com>
2021-01-15 23:29:30 -0300

    console

---
+++ JAVASCRIPT-N1/JS/console.js
@@ -0,0 +1,46 @@
+//console.assert(5<3);
+
+/*console.time()
+
+function sumar(n1, n2) {
+
+	document.write(n1,n2);
+	console.count();//me dice cuantas veces se ejecuta esta funcion
+	console.timeLog() //para saber cuanto timepo tarda en ejecutarse
+}
+
+
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+
+console.countReset();
+
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+sumar(2,4);
+
+console.timeEnd()*/
+
+console.log("%chola mundo","color:red;background:black;padding:20px;border:3px solid blue");
+
